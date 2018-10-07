6e2bff2667eaa50cd4a2f310891f3764d2ca7fbc
alex <alex@isreally.cool>
2018-10-06 18:35:30 -0600

    comments

--- terraform/config.tf
+++ terraform/config.tf
@@ -2,9 +2,9 @@ data "aws_availability_zones" "available" {}
 
 locals {
   azs              = ["${data.aws_availability_zones.available.names}"]
-  environment_name = "prod"
+  environment_name = "prod" # used for resource tagging
   vpc_cidr         = "10.1.0.0/16"
   vpc_name         = "my-vpc"
-  aws_region = "us-west-2"
-  cluster_name = "test-eks-cluster"
+  aws_region       = "us-west-2"
+  cluster_name     = "test-eks-cluster"
 }
