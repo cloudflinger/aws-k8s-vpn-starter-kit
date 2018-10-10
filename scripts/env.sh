a28d5189b0eff3c816837a5ecbfb38063cbbccc7
alex <alex@isreally.cool>
2018-10-09 22:25:58 -0600

    works without export statements

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-export CLUSTER_NAME=test-eks-cluster
-export ENVIRONMENT_NAME=prod
-export VPC_CIDR=10.1.0.0/16
-export VPC_NAME=my-vpc
-export AWS_REGION=us-west-2
-export OVPN_K8S_POD_NETWORK=172.16.0.0
+CLUSTER_NAME=test-eks-cluster
+ENVIRONMENT_NAME=prod
+VPC_CIDR=10.1.0.0/16
+VPC_NAME=my-vpc
+AWS_REGION=us-west-2
+OVPN_K8S_POD_NETWORK=172.16.0.0
+OVPN_K8S_POD_SUBNET=255.240.0.0
